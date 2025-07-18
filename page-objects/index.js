a97fbe848503b324f886bddfc98fdb8bbd46dba9
Alexandra Kiss <kiss.alexandra.dora@gmail.com>
2025-07-18 17:49:38 +0200

    use selectors from page objects

--- page-objects/index.js
+++ page-objects/index.js
@@ -1,3 +1,27 @@
 const selectors = require('./selectors.json');
 
-module.exports = selectors;
+const mainPageSelector = `${selectors.mainPage.selector}`;
+const headerSelector = `${mainPageSelector} ${selectors.mainPage.children.header.selector}`;
+const logoSelector = `${headerSelector} ${selectors.mainPage.children.header.children.logo}`;
+const languageSelector = `${headerSelector} ${selectors.mainPage.children.header.children.language.selector}`;
+const selectedLanguageSelector = `${languageSelector} ${selectors.mainPage.children.header.children.language.children.selectedLanguage}`;
+const welcomeMessageSelector = `${mainPageSelector} ${selectors.mainPage.children.welcomeMessage}`;
+const loginButtonSelector = `${headerSelector} ${selectors.mainPage.children.header.children.loginButton}`;
+const loginFormSelector = `${mainPageSelector} ${selectors.mainPage.loginForm.selector}`;
+const usernameFieldSelector = `${loginFormSelector} ${selectors.mainPage.loginForm.children.usernameField}`;
+const passwordFieldSelector = `${loginFormSelector} ${selectors.mainPage.loginForm.children.passwordField}`;
+const formLoginButtonSelector = `${loginFormSelector} ${selectors.mainPage.loginForm.children.loginButton}`;
+const userProfileSelector = `${mainPageSelector} ${selectors.mainPage.children.header.children.userProfile}`;
+
+module.exports = {
+    logoSelector,
+    languageSelector,
+    selectedLanguageSelector,
+    welcomeMessageSelector,
+    loginButtonSelector,
+    loginFormSelector,
+    usernameFieldSelector,
+    passwordFieldSelector,
+    formLoginButtonSelector,
+    userProfileSelector
+};
--- tests/tests-for-moviedb.spec.ts
+++ tests/tests-for-moviedb.spec.ts
@@ -1,15 +1,21 @@
 import { test, expect } from '@playwright/test';
 import dotenv from 'dotenv';
 import path from 'path';
-
-//TODO: need to investigate and fix this issues with the selectors
-//const selectors = require('./../page-objects/index.js');
+import {
+    logoSelector,
+    languageSelector,
+    selectedLanguageSelector,
+    welcomeMessageSelector,
+    loginButtonSelector,
+    usernameFieldSelector,
+    passwordFieldSelector,
+    formLoginButtonSelector,
+    userProfileSelector
+} from './../page-objects/index.js';
 
 const envVariables = dotenv.config({ path: path.resolve(__dirname, './../.env') });
 const username = envVariables.parsed?.username;
 const password = envVariables.parsed?.password;
-//console.log("icon", selectors.logo);
-//console.log("icon", selectors.mainPage.header.logo);
 
 test.beforeEach(async ({ page }) => {
   await page.goto('https://www.themoviedb.org/');
@@ -19,37 +25,37 @@ test.beforeEach(async ({ page }) => {
 test.describe('tests for moviedb', () => {
     test('the page should be loaded', async ({ page }) => {
       //visibility check for welcome message, icon in the header
-      await expect(page.locator('.page_wrap .title h2')).toBeVisible();
-      await expect(page.locator('.page_wrap header .logo')).toBeVisible();
+      await expect(page.locator(welcomeMessageSelector)).toBeVisible();
+      await expect(page.locator(logoSelector)).toBeVisible();
     });
 
     test('the selected language should be EN', async ({ page }) => {
-      await expect(page.locator('.page_wrap header .translate')).toBeVisible();
-      await expect(page.locator('.page_wrap header .translate div')).toHaveText("en");
+      await expect(page.locator(languageSelector)).toBeVisible();
+      await expect(page.locator(selectedLanguageSelector)).toHaveText("en");
     });
 
     test('login', async ({ page }) => {
       //visibility check for login button
-      await expect(page.locator('.page_wrap header [href="/login"]')).toBeVisible();
-      await page.locator('.page_wrap header [href="/login"]').click();
+      await expect(page.locator(loginButtonSelector)).toBeVisible();
+      await page.locator(loginButtonSelector).click();
 
       //login page should be loaded
       await expect(page).toHaveURL('https://www.themoviedb.org/login');
 
       //add username and password
-      await expect(page.locator('.page_wrap #login_form #username')).toBeVisible();
-      await page.locator('.page_wrap #login_form #username').fill(username as string);
+      await expect(page.locator(usernameFieldSelector)).toBeVisible();
+      await page.locator(usernameFieldSelector).fill(username as string);
 
-      await expect(page.locator('.page_wrap #login_form #password')).toBeVisible();
-      await page.locator('.page_wrap #login_form #password').fill(password as string);
+      await expect(page.locator(passwordFieldSelector)).toBeVisible();
+      await page.locator(passwordFieldSelector).fill(password as string);
 
       //click on the button
-      await expect(page.locator('.page_wrap #login_form #login_button')).toBeVisible();
-      await page.locator('.page_wrap #login_form #login_button').click();
+      await expect(page.locator(formLoginButtonSelector)).toBeVisible();
+      await page.locator(formLoginButtonSelector).click();
 
       //observe that the user is logged in
       const userProfileUrl = "https://www.themoviedb.org/u/" + username;
       await page.waitForURL(userProfileUrl);
-      await expect(page.locator('.page_wrap header .user')).toBeVisible();
+      await expect(page.locator(userProfileSelector)).toBeVisible();
     });
 });
